22735f2d0de5bbd392108e494e7c03300629962c
Ak4tetsu <ak4tetsu@gmail.com>
2024-06-17 22:11:07 +0330

    i had an error trying to send a contex from blog.views to blog.templates.ditail.html that my context was full befor sending and empty inside the ditail.html. still D2

--- shop/blog/views.py
+++ shop/blog/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+from blog.models import Post
+
 def index(request):
-    return HttpResponse('Hello World!!!')
+    latest_posts_list = Post.objects.order_by('-poblish')[:5]
+    # output = ',\n'.join([p.slug for p in latest_posts_list])
+    template = loader.get_template('index.html')
+    context = {
+        'latest_posts_list':latest_posts_list,
+    }
+
+    return HttpResponse(template.render(context,request))
 
 def detail(request, post_id):
-    return HttpResponse(f'your post id is ({post_id})')
+    post = get_object_or_404(Post, id = post_id)
+    template = loader.get_template('ditail.html')
+    context = {
+        'post': post,
+    }
+    return HttpResponse(template.render(context,request))
