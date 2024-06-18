7390e47300696f2a245d50d5e2fe2d309efa2f7b
Ak4tetsu <ak4tetsu@gmail.com>
2024-06-18 23:01:48 +0330

    worked with some of html files and even got to use the bootstrap. D3

--- shop/blog/urls.py
+++ shop/blog/urls.py
@@ -1,11 +1,22 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
 from . import views
+from extensions import converters
 
+
+register_converter(converters.FourDigitYearConverter, 'yyyy')
+
+
+app_name = 'blog_urls'
 urlpatterns = [
-    # examp: hostname/blog/
+    # examp: hostname/blog
     path('',views.index,name='index'),
 
     # examp: hostname/blog/5
-    path('<int:post_id>',views.detail,name='ditail')
+    path('<int:post_id>',views.detail,name='ditail'),
+
+    # examp: hostname/blog/archive/2023
+    # path('archive/<int:year>', views.archive_year, name='archive'),
+    # re_path(r'^archive/(?P<year>[0-9]{4})/$', views.archive_year, name='archive'),
+    path('archive/<yyyy:year>/', views.archive_year, name='archive'),
 ]
--- shop/blog/views.py
+++ shop/blog/views.py
@@ -1,23 +1,40 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+# from django.http import HttpResponse
+# from django.template import loader
 # Create your views here.
 from blog.models import Post
+from django.views.decorators.http import require_http_methods,require_GET,require_POST,require_safe
 
+@require_http_methods(['GET','POST'])
 def index(request):
     latest_posts_list = Post.objects.order_by('-poblish')[:5]
     # output = ',\n'.join([p.slug for p in latest_posts_list])
-    template = loader.get_template('index.html')
+    # template = loader.get_template('index.html')
     context = {
         'latest_posts_list':latest_posts_list,
     }
 
-    return HttpResponse(template.render(context,request))
+    # return HttpResponse(template.render(context,request))
+    return render(request, 'index.html', context)
 
+@require_GET
 def detail(request, post_id):
     post = get_object_or_404(Post, id = post_id)
-    template = loader.get_template('ditail.html')
+    # template = loader.get_template('ditail.html')
     context = {
         'post': post,
     }
-    return HttpResponse(template.render(context,request))
+    # return HttpResponse(template.render(context,request))
+    return render(request, 'ditail.html', context)
+
+@require_safe
+def archive_year(request, year):
+
+    # year_filter_posts = Post.objects.filter(poblish__year = year)
+    year_filter_posts = get_list_or_404(Post, poblish__year = year)
+
+    context = {
+        'year_filter_posts' : year_filter_posts,
+    }
+
+    return render(request, 'archive.html', context)
