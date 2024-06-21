d8b5bdc15d805732a9d844f37210ea33e9e1f3ea
Ak4tetsu <ak4tetsu@gmail.com>
2024-06-21 20:57:46 +0330

    got to work with css and js in django and also used decorators in blog.admin. D4

--- shop/blog/admin.py
+++ shop/blog/admin.py
@@ -4,10 +4,14 @@ from django.contrib import admin
 
 from .models import Post
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'poblish', 'status')
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('poblish', 'status')
     search_fields = ('title', 'body')
     ordering = ['status','poblish']
-admin.site.register(Post, PostAdmin)
+
+
+
+# admin.site.register(Post, PostAdmin)
