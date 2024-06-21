267e766685e3256bffa15de4838f8403eb96baae
Ak4tetsu <ak4tetsu@gmail.com>
2024-06-22 00:46:38 +0330

    worked with blog/admin translations to farsi. D5LD

--- shop/blog/admin.py
+++ shop/blog/admin.py
@@ -1,16 +1,41 @@
 from django.contrib import admin
-
+from django.http import HttpResponse
+from django.core import serializers
 # Register your models here.
 
 from .models import Post
 
+def make_posts_published(modeladmin, request, queryset):
+    resault = queryset.update(status = 'published')
+    modeladmin.message_user(request, f'{resault} post(s) were successfully marked as published')
+
+# make_posts_published.short_description = 'Make selected post(s) published'
+make_posts_published.short_description = 'انتشار دادن پست ها'
+
+
+def make_posts_draft(modeladmin, request, queryset):
+    resault = queryset.update(status = 'draft')
+    modeladmin.message_user(request, f'{resault} post(s) were successfully marked as draft')
+
+# make_posts_draft.short_description = 'Make selected post(s) draft'
+make_posts_draft.short_description = 'خارج کردن از حالت انتشار'
+
+def export_as_json(modeladmin, request, queryset):
+    response = HttpResponse(content_type = "application/json")
+    serializers.serialize("json", queryset, stream = response)
+    return response
+
+# export_as_json.short_description = 'Export selected posts as json'
+export_as_json.short_description = 'برگرداندن به عنوان json'
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'poblish', 'status')
+    list_display = ('title', 'slug', 'publish', 'status')
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('poblish', 'status')
+    list_filter = ('publish', 'status')
     search_fields = ('title', 'body')
-    ordering = ['status','poblish']
+    ordering = ['status','publish']
+    actions = [make_posts_published, make_posts_draft, export_as_json,]
 
 
 
--- shop/blog/models.py
+++ shop/blog/models.py
@@ -2,18 +2,25 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
 class Post(models.Model):
     STATUS_CHOICES = (
-        ('draft','Draft'),
-        ('published','Published'),
+        ('draft','در انتظار'),
+        ('published','انتشار یافته'),
     )
-    title = models.CharField(max_length=60)
-    slug = models.SlugField(max_length=100)
-    body = models.TextField()
-    poblish = models.DateTimeField(default= timezone.now)
-    created = models.DateTimeField(auto_now_add= True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=60, choices=STATUS_CHOICES, default= 'draft')
+    title = models.CharField(max_length=60, verbose_name= "عنوان")
+    slug = models.SlugField(max_length=100, verbose_name="عنوان پیوسته")
+    body = models.TextField(verbose_name="متن کامل")
+    publish = models.DateTimeField(default= timezone.now, verbose_name="انتشار")
+    created = models.DateTimeField(auto_now_add= True, verbose_name="ساخت")
+    updated = models.DateTimeField(auto_now=True, verbose_name="ویرایش")
+    status = models.CharField(max_length=60, choices=STATUS_CHOICES, default= 'draft', verbose_name="وضعیت")
+
+    class Meta:
+        verbose_name = 'پست'
+        verbose_name_plural = "پست ها"
+
+
 
     def __str__(self) -> str:
         return f'Post object(id = {self.id}, title = {self.title})'
